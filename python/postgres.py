668e573437ad1cf918730a1afcecd41d97511aa5
beep1000101 <matiponiatowski@o2.pl>
2025-03-21 15:52:58 +0100

    refactored postgres script

--- python/postgres.py
+++ python/postgres.py
@@ -1,19 +1,23 @@
-from sqlalchemy import create_engine
-from sqlalchemy import exc
+import os
+import pandas as pd
+
+from sqlalchemy import create_engine, exc, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 
-import pandas as pd
+from dotenv import load_dotenv
 
 from read import get_orders_directory, get_users_directory, get_csvs, merge_csvs
 
-# Replace with your actual username, password, and database name
-DATABASE_URL_TEMPLATE = "postgresql+psycopg2://{username}:{password}@localhost/{database_name}"
+load_dotenv()
+
+DATABASE_URL_TEMPLATE = "postgresql+psycopg2://{username}:{password}@{db_host}/{database_name}"
+
 DATABASE_URL = DATABASE_URL_TEMPLATE.format(
-    username="postgres",
-    password="postgrespassword",
-    database_name="bgddatabase"
+    username=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    db_host=os.getenv("DB_HOST"),
+    database_name=os.getenv("DB_NAME"),
 )
 
 # Create the SQLAlchemy engine
@@ -25,17 +29,14 @@ Base = declarative_base()
 # Session maker to create database session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Constants for table names
+USERS_TABLE = "users"
+ORDERS_TABLE = "orders"
+
 
 class User(Base):
-    """
-    Represents a user in the database.
-
-    Attributes:
-        identifier (int): The unique identifier for the user. Primary key.
-        first_name (str): The first name of the user. Indexed for faster queries.
-        last_name (str): The last name of the user. Must be unique and indexed.
-    """
-    __tablename__ = 'users'
+    """Represents a user in the database."""
+    __tablename__ = USERS_TABLE
 
     identifier = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, index=True)
@@ -46,21 +47,12 @@ class User(Base):
 
 
 class Orders(Base):
-    """
-    Represents an order in the database.
-
-    Attributes:
-        order_id (int): The unique identifier for the order. Primary key.
-        customer_id (int): The identifier of the customer who placed the order. Foreign key referencing the User table.
-        product (str): The name of the product ordered. Indexed.
-        quantity (int): The quantity of the product ordered. Indexed.
-        total_price (int): The total price of the order. Indexed.
-        order_date (str): The date the order was placed. Indexed.
-    """
-    __tablename__ = 'orders'
+    """Represents an order in the database."""
+    __tablename__ = ORDERS_TABLE
 
     order_id = Column(Integer, primary_key=True, index=True)
-    customer_id = Column(Integer, ForeignKey('users.identifier'), index=True)
+    customer_id = Column(Integer, ForeignKey(
+        f"{USERS_TABLE}.identifier"), index=True)
     product = Column(String, index=True)
     quantity = Column(Integer, index=True)
     total_price = Column(Integer, index=True)
@@ -71,88 +63,116 @@ class Orders(Base):
 
 
 # Create tables in the database
-Base.metadata.create_all(bind=engine)
+def initialize_database():
+    """Initialize the database by creating all tables."""
+    Base.metadata.create_all(bind=engine)
 
 
-# Function to interact with the database (add user)
-def create_user(
-        db_session,
-        identifier: int,
-        first_name: str,
-        last_name: str,
-):
-    db_user = User(
-        identifier=identifier,
-        first_name=first_name,
-        last_name=last_name)
-    db_session.add(db_user)
-    db_session.commit()
-    db_session.refresh(db_user)
-    return db_user
+# Utility function for session management
+def get_db_session():
+    """Provide a transactional scope around a series of operations."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-def create_order(
-        db_session,
-        customer_id: int,
-        product: str,
-        quantity: int,
-        total_price: int,
-        order_date: str,
-):
-    db_order = Orders(
-        customer_id=customer_id,
-        product=product,
-        quantity=quantity,
-        total_price=total_price,
-        order_date=order_date)
-    db_session.add(db_order)
-    db_session.commit()
-    db_session.refresh(db_order)
-    return db_order
+# CRUD Operations
+def create_user(db_session, identifier: int, first_name: str, last_name: str):
+    """Add a single user to the database."""
+    try:
+        db_user = User(identifier=identifier,
+                       first_name=first_name, last_name=last_name)
+        db_session.add(db_user)
+        db_session.commit()
+        db_session.refresh(db_user)
+        return db_user
+    except exc.SQLAlchemyError as e:
+        db_session.rollback()
+        print(f"Error creating user: {e}")
+        return None
 
 
-def create_users_bulk(db_session, users_csv: pd.DataFrame):
-    records = users_csv.to_dict(orient='records')
+def create_order(db_session, customer_id: int, product: str, quantity: int, total_price: int, order_date: str):
+    """Add a single order to the database."""
+    try:
+        db_order = Orders(
+            customer_id=customer_id,
+            product=product,
+            quantity=quantity,
+            total_price=total_price,
+            order_date=order_date
+        )
+        db_session.add(db_order)
+        db_session.commit()
+        db_session.refresh(db_order)
+        return db_order
+    except exc.SQLAlchemyError as e:
+        db_session.rollback()
+        print(f"Error creating order: {e}")
+        return None
 
-    db_session.bulk_insert_mappings(User, records)
-    db_session.commit()
+
+def bulk_insert_users(db_session, users_csv: pd.DataFrame):
+    """Bulk insert users from a DataFrame."""
+    try:
+        records = users_csv.to_dict(orient='records')
+        db_session.bulk_insert_mappings(User, records)
+        db_session.commit()
+    except exc.SQLAlchemyError as e:
+        db_session.rollback()
+        print(f"Error bulk inserting users: {e}")
 
 
-def create_orders_bulk(db_session, orders_csv: pd.DataFrame):
-    records = orders_csv.to_dict(orient='records')
+def bulk_insert_orders(db_session, orders_csv: pd.DataFrame):
+    """Bulk insert orders from a DataFrame."""
+    try:
+        records = orders_csv.to_dict(orient='records')
+        db_session.bulk_insert_mappings(Orders, records)
+        db_session.commit()
+    except exc.SQLAlchemyError as e:
+        db_session.rollback()
+        print(f"Error bulk inserting orders: {e}")
 
-    db_session.bulk_insert_mappings(Orders, records)
-    db_session.commit()
 
-
-def get_users(db_session):
+def get_all_users(db_session):
+    """Retrieve all users from the database."""
     return db_session.query(User).all()
 
 
-def get_orders(db_session):
+def get_all_orders(db_session):
+    """Retrieve all orders from the database."""
     return db_session.query(Orders).all()
 
 
-def drop_users_table(db_session):
-    User.__table__.drop(db_session.bind)
+def drop_table(db_session, table):
+    """Drop a specific table."""
+    try:
+        table.__table__.drop(db_session.bind)
+    except exc.SQLAlchemyError as e:
+        print(f"Error dropping table: {e}")
 
 
-def drop_orders_table(db_session):
-    Orders.__table__.drop(db_session.bind)
+# Main function to run the script
+def main():
+    initialize_database()
 
-
-# Main function to run code
-if __name__ == "__main__":
     with SessionLocal() as db:
+        # Load and merge user data
         users_directory = get_users_directory()
         users_csvs = get_csvs(users_directory)
         users_df = merge_csvs(users_csvs)
 
-        # get orders data
+        # Load and merge order data
         orders_directory = get_orders_directory()
         orders_csvs = get_csvs(orders_directory)
         orders_df = merge_csvs(orders_csvs)
 
-        # put data into database
-        create_users_bulk(db, users_df)
-        create_orders_bulk(db, orders_df)
+        # Insert data into the database
+        bulk_insert_users(db, users_df)
+        bulk_insert_orders(db, orders_df)
+
+
+if __name__ == "__main__":
+    main()
