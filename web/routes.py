1cd1a55f0c4bd3aff913c9bdb996d3fcb596815d
beep1000101 <matiponiatowski@o2.pl>
2025-03-22 13:46:35 +0100

    add HTTP enum for standardized request methods and update routes to use it

--- web/routes.py
+++ web/routes.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, request
+from web.enums import HTTP
 
 app_routes = Blueprint('app_routes', __name__)
 
 
-@app_routes.route('/not_dead', methods=['POST', 'GET'])
+@app_routes.route('/not_dead', methods=[HTTP.POST, HTTP.GET])
 def not_dead():
     """Testing request, to be terminated later."""
-    if request.method == 'POST':
+    if request.method == HTTP.POST:
         result = f'Not dead, {request.method}'
         return result
-    elif request.method == 'GET':
+    elif request.method == HTTP.GET:
         result = f'Not dead, {request.method}'
         return result
     else:
